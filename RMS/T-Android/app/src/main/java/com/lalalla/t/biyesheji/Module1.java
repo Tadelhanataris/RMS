3d2c6c5679a0dfee02ee888b4e6190d80e6e693b
Tadelaide <wlt582457946@vip.qq.com>
2016-03-02 00:22:52 +0800

    第一次提交
    1

---
+++ RMS/T-Android/app/src/main/java/com/lalalla/t/biyesheji/Module1.java
@@ -0,0 +1,14 @@
+package com.lalalla.t.biyesheji;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Module1 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_module1);
+    }
+}
