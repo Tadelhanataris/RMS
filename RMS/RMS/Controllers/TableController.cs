6305b3d566b29b96c92e3f9054ae0ca136afa425
Ataris <512056026@qq.com>
2016-03-24 14:15:15 +0800

    11111111

---
+++ RMS/RMS/Controllers/TableController.cs
@@ -0,0 +1,35 @@
+﻿using PagedList;
+using RMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RMS.Controllers
+{
+    public class TableController : Controller
+    {
+        // GET: Table
+        public ActionResult Index(string id)
+        {
+            var context = DBHelper.instence.Table.OrderByDescending(x => x.ID);
+            if (id == "" || id == null)
+            {
+                ViewData.Add("context", context.ToPagedList(1, 10));
+                return View();
+            }
+            int? pagenum = null;
+            try
+            {
+                pagenum = int.Parse(id);
+            }
+            catch
+            {
+                return HttpNotFound();
+            }
+            ViewData.Add("context", context.ToPagedList((pagenum ?? 1), 10));
+            return View();
+        }
+    }
+}
--- RMS/RMS/Models/Attachment.cs
+++ RMS/RMS/Models/Attachment.cs
@@ -3,7 +3,7 @@
     public class Attachment
     {
         public int ID { get; set; }
-        public string href { get; set; }
+        public virtual Files href { get; set; }
         public string filename { get; set; }
         public string filesize { get; set; }
         public string filetype { get; set; }
