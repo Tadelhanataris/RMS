6853ae5edf2e9d55b59a78ad87cac58c23738d64
Tadelaide <wlt582457946@vip.qq.com>
2016-04-13 23:13:48 +0800

    123

--- RMS/T-Android/app/src/main/java/com/lalalla/t/biyesheji/Selfinformation.java
+++ RMS/T-Android/app/src/main/java/com/lalalla/t/biyesheji/Selfinformation.java
@@ -2,9 +2,14 @@ package com.lalalla.t.biyesheji;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
 import android.support.v4.widget.SimpleCursorAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -12,15 +17,16 @@ public class Selfinformation extends AppCompatActivity {
     private ListView listView;
     ProjectDB Selfinformations;
     public static SimpleCursorAdapter adapter;
-
+    private Button btnback;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selfinformation);
         listView = (ListView) findViewById(R.id.selfinformationlist);
+        btnback= (Button) findViewById(R.id.selfinformationbtnback);
         Selfinformations = new ProjectDB(getApplicationContext());
         adapter = new SimpleCursorAdapter(this,
-                R.layout.activity_selfinformation, Selfinformations.getselfinformaitonAllData(),
+                R.layout.selfinformationitem, Selfinformations.getselfinformaitonAllData(),
                 new String[] { "_id",
                         "studentname",
                         "studentnumber",
@@ -43,6 +49,25 @@ public class Selfinformation extends AppCompatActivity {
                         R.id.selfinformationphone} );
         listView.setAdapter(adapter);
 
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent();
+                intent.setClass(Selfinformation.this, Selfinformationedit.class);
+                int sid = ((Cursor) (adapter.getItem(position))).getInt(((Cursor) (adapter.getItem(position))).getColumnIndex("_id"));
+                intent.putExtra("id", sid);
+                Selfinformation.this.startActivity(intent);
+            }
+        });
+
+        btnback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
 
---
+++ RMS/T-Android/app/src/main/java/com/lalalla/t/biyesheji/Selfinformationedit.java
@@ -0,0 +1,13 @@
+package com.lalalla.t.biyesheji;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Selfinformationedit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selfinformationedit);
+    }
+}
