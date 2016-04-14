3234c3acf3dee19081ef965c0e37933316477ce6
Ataris <512056026@qq.com>
2016-04-14 22:13:06 +0800

    newProject Model

--- RMS/RMS/Models/ListViewModel.cs
+++ RMS/RMS/Models/ListViewModel.cs
@@ -47,6 +47,55 @@ namespace RMS.Models
         public virtual File file { get; set; }
     }
 
+    public class Academy
+    {
+        public int ID { get; set; }
+        public string academy { get; set; }
+    }
+
+    public class Member
+    {
+        public int ID { get; set; }
+        public string name { get; set; }
+        public bool sex { get; set; }
+        public long sno { get; set; }
+        public Academy academy { get; set; }
+        public string subject { get; set; }
+        public string identity { get; set; }
+        public string email { get; set; }
+        public long phone { get; set; }
+    }
+
+    public class Fund
+    {
+        public enum m_type { tszl,sycl,dy,ys,cgzh}
+        public int ID { get; set; }
+        public m_type fundType { get; set; }
+        public float money { get; set; }
+        public string reason { get; set; }
+    }
+
+    public class Project
+    {
+        public enum p_type {znl,zxl,cyl,dxzz,yxzz }
+        public int ID { get; set; }
+        public string projectName { get; set; }
+        public p_type projectType { get; set; }
+        public string projectSubject { get; set; }
+        public virtual Member leader { get; set; }
+        public virtual Member[] members { get; set; }
+        //todo 教师类
+        //public virtual Teacher teacher { get; set; }
+        public string baseon { get; set; }
+        public string purpose { get; set; }
+        public string schedule { get; set; }
+        public string format { get; set; }
+        public string special { get; set; }
+        public string experience { get; set; }
+        public Fund[5] fund { get; set; }
+    }
+
+
     public class Project_Progress
     {
         
@@ -54,8 +103,11 @@ namespace RMS.Models
         public int sequence { get; set; }
         public string title { get; set; }
         public string content { get; set; }
-        //public virtual Project project { get; set; }
+        public virtual Project project { get; set; }
     }
+
+
+
 
     public class DBHelper : DbContext
     {
@@ -68,6 +120,8 @@ namespace RMS.Models
         public DbSet<Policy> Policy { get; set; }
         public DbSet<Table> Table { get; set; }
         public DbSet<Project_Progress> Project_progress { get; set; }
+
+        public DbSet<Academy> Academy { get; set; }
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();  
